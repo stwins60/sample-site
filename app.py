86ad1e7fcca1eb0265e7fc7372fcc04f0f973b97
Idris Fagbemi <idrisniyi94@gmail.com>
2021-11-28 18:45:50 -0600

    First Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+
+
+my_personal_page = Flask(__name__)
+
+FLASK_ENV = 'development'
+FLASK_DEBUG = True
+
+
+
+@my_personal_page.route('/')
+def home():
+    return render_template('index.html')
+
+@my_personal_page.route('/about')
+def about():
+    return render_template('about.html')
+
+@my_personal_page.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@my_personal_page.route('/sitemap')
+def sitemap():
+    return render_template('sitemap.html')
+
+
+if __name__ == '__main__':
+    my_personal_page.run()
